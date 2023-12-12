bb07487e6e9a2d9c69190a1e5d9c6f5e80421f70
Kirk Davies <kdavies@marchnetworks.com>
2023-12-11 20:14:07 -0500

    Auto-generated commit [ci skip]

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-AMS3QFHB.mjs";import"./chunk-X5YCR2ND.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/constants.ts
+++
@@ -1,4 +0,0 @@
-export const APP_NAME = 'App Name';
-export const PHONE_NUMBER = '+1 (555) 555-5555';
-
-export const EMAIL = 'info@company.com';
